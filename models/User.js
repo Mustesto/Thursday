508f60cb1da80687a32a141a7d575220a84389b7
Mustesto <testlovableai@gmail.com>
2026-02-26 21:34:02 +0000

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,17 @@
 
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    minlength: 2
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    match: [/.+@.+\..+/, "Email must be valid"]
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
