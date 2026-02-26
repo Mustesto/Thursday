c92ab25686fd0c3add9c11373097cfd4442ba48a
Mustesto <testlovableai@gmail.com>
2026-02-26 07:32:32 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-
 const app = express();
-app.use(cors());
-app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log("MongoDB connecté"))
-  .catch(err => console.log("Erreur MongoDB:", err.message));
-
-app.get("/", (req, res) => res.send("API fonctionne 🚀"));
+app.get("/", (req, res) => res.send("Railway OK 🚀"));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Serveur démarré sur port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
